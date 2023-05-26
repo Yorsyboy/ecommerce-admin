02e3ef080ab2a49d435c89e5aeb1230b9c611f31
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-05-26 18:37:49 +0100

    project setup and google auth

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Home() {
+  const { data: session } = useSession()
+  if (!session) {
+    return (
+      <>
+        <div className="bg-blue-900 w-screen h-screen flex items-center">
+          <div className="text-center w-full">
+            <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">
+              Sign In with Google
+            </button>
+          </div>
+        </div>
+
+      </>
+    )
+  }
+  return (
+    <>
+      Signed in as {session.user.email} <br />
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  )
+}
